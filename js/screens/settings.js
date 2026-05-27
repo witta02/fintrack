27c5871cf0221d77111c67122c7c7880eea8c37b
VitaMinZ <151541712+witta02@users.noreply.github.com>
2026-05-27 10:15:53 +0700

    Add files via upload

--- js/screens/settings.js
+++ js/screens/settings.js
@@ -178,7 +178,7 @@ function setupEventListeners(container) {
         });
       }
     } else {
-      alert('เบราว์เซอร์นี้ไม่สนับสนุนการแจ้งเตือนภายในแอป');
+      alert(clearbtn);
     }
   });
 
