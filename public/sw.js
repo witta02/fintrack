e46fd4321514d541cff85a7109de3121759234cd
witta02 <witta8916@gmail.com>
2026-08-06 10:34:46 +0700

    Bump SW cache to v10 and force docked bottom nav layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fintrack-cache-v9";
+const CACHE_NAME = "fintrack-cache-v10";
 const ASSETS = ["/", "/index.html", "/favicon.png", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
